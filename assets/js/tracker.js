74e62872d7c8ea0621dba1e9d399ee0d0c48ab20
Mind & Machine <86833111+AI8V@users.noreply.github.com>
2025-05-15 06:08:38 +0300

    Update tracker.js

--- assets/js/tracker.js
+++ assets/js/tracker.js
@@ -13,7 +13,7 @@
     };
 
     // إرسال البيانات إلى Google Apps Script
-    await fetch("https://script.google.com/macros/s/AKfycbzc7m34sI0f2xNM7NMUOl1g9O2nAJn0Jb8gEWj6GsIhiTPe4nxw9-4nBAm8ghWdlxroMw/exec", {
+    await fetch("https://script.google.com/macros/s/AKfycbyUaThOm163cXvftrUUIbxtv6AsYRLUgq5MVbDTuZw4W8fEnrRTsXKX4vnXloYtgkr_YQ/exec", {
       method: "POST",
       body: JSON.stringify(dataToSend),
       headers: {
